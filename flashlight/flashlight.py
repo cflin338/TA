7fc7e57922dc9f1b0643ed216fbb98940e446f28
clin430 <clin430@gmail.com>
2024-11-12 14:38:55 -0600

    dickson

--- flashlight/flashlight.py
+++ flashlight/flashlight.py
@@ -1,27 +1,41 @@
 import RPi.GPIO as GPIO
-import time, sys
-GPIO.setmode(GPIO.BOARD)
+import time
+import sys
+
+# constants
 BUTTON_0_PIN = 16
 LED_0_PIN = 18
-cur_state = 0
-GPIO.setwarnings(False)
-GPIO.setup(LED_0_PIN, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(BUTTON_0_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(BUTTON_0_PIN, GPIO.FALLING)
+
+# gpio setup
+GPIO.setmode(GPIO.BOARD)  # physical pins
+GPIO.setup(BUTTON_0_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # button is pulldown
+GPIO.setup(LED_0_PIN, GPIO.OUT)  # led is output
+
+# enum for led state
+# LED_STATE = ['LED_OFF', 'LED_ON']
+# led = enumerate(LED_STATE)
+led = False
+
+# basic edge detection
+current_state = None
+prev_state = GPIO.LOW
+
 try:
     while True:
-        time.sleep(0.01)
-        if GPIO.event_detected(BUTTON_0_PIN):
-            if cur_state == 0:
-                print("Light On")
-                GPIO.output(LED_0_PIN, GPIO.HIGH)
-                cur_state = 1
-            else:
-                print("Light Off")
-                GPIO.output(LED_0_PIN,GPIO.LOW)
-                cur_state = 0
+        # button check
+        current_state = GPIO.input(BUTTON_0_PIN)
+        if current_state == GPIO.HIGH:
+            if prev_state == GPIO.LOW:
+                led = not led
+                GPIO.output(LED_0_PIN, led)
+            
+        prev_state = current_state
+    # sleep for CPU usage
+    time.sleep(0.01)
+
 except KeyboardInterrupt:
-    print("\nKeyboard interrupt")
+    # cleanup enacted by ctrl+c
+    print("Got Keyboard Interrupt. Cleaning up and exiting.")
     GPIO.output(LED_0_PIN, GPIO.LOW)
     GPIO.cleanup()
     sys.exit()
