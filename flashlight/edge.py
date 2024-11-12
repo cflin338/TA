8baa4b245dd4a876c9f180f2e74857633ea837ad
clin430 <clin430@gmail.com>
2024-11-12 14:35:18 -0600

    ed

--- flashlight/edge.py
+++ flashlight/edge.py
@@ -9,20 +9,20 @@ GPIO.setup(LED_0_PIN, GPIO.OUT, initial = GPIO.LOW)
 GPIO.setup(BUTTON_0_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(BUTTON_0_PIN, GPIO.FALLING)
 try:
-while True:
-    edge = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING,bouncetime=10)
-    if edge is not None:
-        if cur_state == 0:
-            print("Light On")
-            GPIO.output(LED_0_PIN, GPIO.HIGH)
-            cur_state = 1
-        else:
-            print("Light Off")
-            GPIO.output(LED_0_PIN,GPIO.LOW)
-            cur_state = 0
-        edge = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.FALLING,bouncetime=10)
-except KeyboardInterrupt:
-    print("\nKeyboard interrupt")
-    GPIO.output(LED_0_PIN, GPIO.LOW)
-    GPIO.cleanup()
-    sys.exit()
+    while True:
+        edge = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING,bouncetime=10)
+        if edge is not None:
+            if cur_state == 0:
+                print("Light On")
+                GPIO.output(LED_0_PIN, GPIO.HIGH)
+                cur_state = 1
+            else:
+                print("Light Off")
+                GPIO.output(LED_0_PIN,GPIO.LOW)
+                cur_state = 0
+            edge = GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.FALLING,bouncetime=10)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt")
+        GPIO.output(LED_0_PIN, GPIO.LOW)
+        GPIO.cleanup()
+        sys.exit()
