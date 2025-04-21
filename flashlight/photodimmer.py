17184a4e9f86fc1033c8da79e9185ae9ae7f0ae5
clin338 <clin430@gmail.com>
2025-04-21 15:26:43 -0500

    pen

--- flashlight/photodimmer.py
+++ flashlight/photodimmer.py
@@ -1,41 +1,75 @@
+import spidev
+import RPi.GPIO as GPIO
 import time
 import sys
 import os
-import RPi.GPIO as GPIO
-import spidev
-
-spi = spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz = 100000
-
 sys.path.insert(0, '../utilities')
 import utilities
 
-BUTTON_0_PIN = 16
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON_0_PIN, GPIO.IN)
+def getLightLevel():
+    try:
+        #Stall until rising edge detected
+        GPIO.wait_for_edge(BREADBOARD_BUTTON_PIN, GPIO.RISING)
+        #Only runs in this case
+        time.sleep(1)
+        to_send = [0b00000001, 0b10000000, 0b00000000]
+        #Read device
+        dataList = spi.xfer(to_send)
+        return (256 * dataList[1] + dataList[2])
+    except KeyboardInterrupt:
+        endProgram()
 
-pwm = utilities.HW_PWM(2000)
-
-print("Cover the photosensor and press the push button")
-GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, bouncetime=500)
-returned_value = spi.xfer([0x01, 0b10000000, 0x00])
-dark_ADC_value = ((returned_value[1] << 8) + (returned_value[2]) & 0b0000001111111111)
-
-print("Shine a flashlight on the photosensor and press the push button")
-GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, bouncetime=500)
-returned_value = spi.xfer([0x01, 0b10000000, 0x00])
-light_ADC_value = ((returned_value[1] << 8) + (returned_value[2]) & 0b0000001111111111)
-
-try:
-    while True:
-        returned_value = spi.xfer([0x01, 0b10000000, 0x00])
-        ADC_value = ((returned_value[1] << 8) + (returned_value[2]) & 0b0000001111111111)
-        input_voltage = ADC_value/1023*3.3
-        duty_cycle_current = (ADC_value - light_ADC_value)/(dark_ADC_value - light_ADC_value)*100
-        pwm.set_duty_cycle(duty_cycle_current)
-        time.sleep(0.01)
-except KeyboardInterrupt:
+def endProgram():
     print('Got Keyboard Interrupt. Cleaning up and exiting')
     pwm.set_duty_cycle(0.0)
+    GPIO.cleanup()
     sys.exit()
+
+# "Macros"
+BUS = 0
+DEVICE = 0
+MAX_SPEED_HZ = 100000
+BYTES_TO_READ = 36
+BREADBOARD_LED_PIN = 12
+BREADBOARD_BUTTON_PIN = 16
+
+# Settings
+spi = spidev.SpiDev()
+spi.open(BUS, DEVICE)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BREADBOARD_BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+spi.max_speed_hz = MAX_SPEED_HZ
+spi.mode = 0b01
+
+#Initialize running vars
+state = 0
+# Create PWM object
+pwm = utilities.HW_PWM(2000)
+
+#Get dark val
+print("Cover the photosensor and press the push button")
+darkVal = getLightLevel()
+#print("DARKVAL: " + str(darkVal))
+
+#Get light val
+print("Shine a flashlight on the photosensor and press the push button")
+lightVal = getLightLevel()
+#print("LIGHTVAL: " + str(lightVal))
+
+try:
+    while(1):
+        #8 start bits then SGL/DIFF, D2, D1, D0. Select CH0 by sending 0, 0, 0
+        to_send = [0b00000001, 0b10000000, 0b00000000]
+        #Read device
+        dataList = spi.xfer(to_send)
+        #Convert list to voltage val (no idea why 310 works here)
+        adcVal = (256 * dataList[1] + dataList[2])
+        PWMVal = (100 / (darkVal - lightVal)) * adcVal
+        if(PWMVal > 100):
+            PWMVal = 100
+        elif(PWMVal < 0):
+            PWMVal = 0
+        pwm.set_duty_cycle(PWMVal)
+        time.sleep(.01)
+except KeyboardInterrupt:
+    endProgram()
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -2,6 +2,7 @@ import time
 import sys
 import os
 PWM_PATH = "/sys/class/pwm/pwmchip0"
+
 class HW_PWM:
     def __init__(self, frequency):
         self.frequency = frequency
@@ -14,33 +15,35 @@ class HW_PWM:
         print(export_cmd)
         os.system(export_cmd)
         time.sleep(0.5)
-
+        
         print('Set the period')
         period = int(1000000000 / frequency)
         period_cmd = "echo " + str(period) + " > " + PWM_PATH + "/pwm0/period"
         ##print(period_cmd)
         os.system(period_cmd)
         time.sleep(0.5)
+        
         print('Set duty cycle to 0')
         duty_cycle_cmd = "echo 0 > " + PWM_PATH + "/pwm0/duty_cycle"
         #print(duty_cycle_cmd)
         os.system(duty_cycle_cmd)
         time.sleep(0.5)
+        
         print('Enable PWM0')
         print(enable_cmd)
         os.system(enable_cmd)
         time.sleep(0.5)
-
-    def set_duty_cycle(self, duty_cycle_percent):
-    # TODO: Complete this function
-        if duty_cycle_percent > 100:
-            duty_cycle_percent = 100
-        if duty_cycle_percent <= 0:
-            duty_cycle_percent = 0
-
         
-
-        echo_value = duty_cycle_percent*5000
-        duty_cycle_cmd = "echo " + str(int(echo_value)) + " > " + PWM_PATH + "/pwm0/duty_cycle" 
-        print(duty_cycle_cmd)
+    def set_duty_cycle(self, duty_cycle_percent):
+        if(duty_cycle_percent > 100):
+            duty_cycle_percent = 100
+        elif(duty_cycle_percent < 0):
+            duty_cycle_percent = 0
+            
+        dutyCycleNum = 5000 * duty_cycle_percent
+        self.duty_cycle = dutyCycleNum
+        self.duty_cycle_percent = duty_cycle_percent
+        
+        duty_cycle_cmd = "echo " + str(int(dutyCycleNum)) + " > " + PWM_PATH + "/pwm0/duty_cycle"
         os.system(duty_cycle_cmd)
+        
