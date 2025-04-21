6a38db3dde28e9e1f250b03338032aed390d6a70
clin338 <clin430@gmail.com>
2025-04-21 14:55:59 -0500

    correct-check

--- flashlight/photodimmer.py
+++ flashlight/photodimmer.py
@@ -25,9 +25,38 @@ spi.open(bus, device)
 spi.max_speed_hz = 1000000
 to_send = [0x01, 0b10000000, 0x0]
 
-        time.sleep(0.01)
-    
-    
+pwm = utilities.HW_PWM(2000)
+
+print("Cover the photosensor and press the push button")
+GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, bouncetime = 25)
+to_send = [0x01, 0b10000000, 0x0]
+spi_data = spi.xfer(to_send)
+photosensor_max = spi_data[2] + (spi_data[1]*256)
+time.sleep(.1)
+
+print("Shine a flashlight on th ephotosensor and press the push button")
+GPIO.wait_for_edge(BUTTON_0_PIN, GPIO.RISING, bouncetime = 25)
+to_send = [0x01, 0b10000000, 0x0]
+spi_data = spi.xfer(to_send)
+photosensor_min = spi_data[2] + (spi_data[1]*256)
+time.sleep(.1)
+
+
+try:
+    while True:
+        to_send = [0x01, 0b10000000, 0x0]
+        spi_data = spi.xfer(to_send)
+        digital_value = spi_data[2] + (spi_data[1]*256)
+        pwm_value = ((digital_value-photosensor_min) / (photosensor_max-photosensor_min)) * 100
+        if (digital_value > photosensor_max):
+            pwm.set_duty_cycle(100)
+        elif (digital_value < photosensor_min):
+            pwm.set_duty_cycle(0)
+        else:
+            pwm.set_duty_cycle(pwm_value)
+        
+
+
 except KeyboardInterrupt:
     print('Got Keyboard Interrupt. Cleaning up and exiting')
     pwm.set_duty_cycle(0.0)
